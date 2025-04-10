a2fdce998ccda8923eb6b755ff5d2a07c42d17e6
Neil Gorski <neil.gorski@hotmail.ch>
2025-04-10 23:25:49 +0200

    add querySelectorAll

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,11 @@
-console.log('DOM');
+console.log("DOM");
+const elCommentsItems = document.querySelectorAll(
+  ".comments__item, .comments__item--newest"
+);
+if (elCommentsItems) {
+  console.log(elCommentsItems);
+  // const elementsDataInfo = Array.from(elCommentsItems).filter((el) =>
+  //   el.hasAttribute("data-info")
+  // );
+  // console.log(`There are ${elementsDataInfo.length}`);
+}
