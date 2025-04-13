da594e8e5e32c005f39a9b205089b129cf3393c8
Neil Gorski <neil.gorski@hotmail.ch>
2025-04-13 22:43:41 +0200

    04-done

--- 04/app.js
+++ 04/app.js
@@ -1,8 +1,21 @@
-console.log('DOM');
+console.log("DOM");
 
 // struktura do wykorzystania w pętli
 const menuItems = [
-    {text: 'start', url: '/'},
-    {text: 'galeria', url: '/gallery'},
-    {text: 'kontakt', url: '/contact'},
+  { text: "start", url: "/" },
+  { text: "galeria", url: "/gallery" },
+  { text: "kontakt", url: "/contact" },
 ];
+
+const nav = document.querySelector("nav");
+const uList = document.createElement("ul");
+nav.appendChild(uList);
+
+for (let i = 0; i < menuItems.length; i++) {
+  const lItem = document.createElement("li");
+  const aItem = document.createElement("a");
+  lItem.appendChild(aItem);
+  aItem.href = menuItems[i].url;
+  aItem.innerText = menuItems[i].text;
+  uList.appendChild(lItem);
+}
