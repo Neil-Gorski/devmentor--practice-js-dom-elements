23f1cdefebcb7e79a59964cbb8f2809c9abbaf43
Neil Gorski <neil.gorski@hotmail.ch>
2025-04-13 23:14:36 +0200

    05-task 1-3 done

--- 05/app.js
+++ 05/app.js
@@ -1,3 +1,19 @@
-console.log('DOM');
+console.log("DOM");
 
-const curr = document.querySelector('.js-curr');
+const curr = document.querySelector(".js-curr");
+// 1.
+const deletBtn = document.createElement("button");
+deletBtn.innerText = "usuń z koszyka";
+curr.parentElement.appendChild(deletBtn);
+// 2.
+const childArr = [...curr.parentElement.children];
+childArr.forEach((element) => {
+  if (element) {
+    element.classList.add("siblings");
+  }
+});
+// 3.
+curr.parentElement.nextElementSibling.setAttribute(
+  "title",
+  "nextElementSibling"
+);
